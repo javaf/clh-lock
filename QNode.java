74c495b341aa5c65729864461a5100a17706ade8
Subhajit Sahu <wolfram77@gmail.com>
2022-04-08 08:51:14 +0000

    w/ volatile QNode (closes #1)

--- QNode.java
+++ QNode.java
@@ -1,3 +1,8 @@
 class QNode {
-  boolean locked = false;
+  // This variable should ideally be declared
+  // volatile. If however this variable is
+  // only checked once per resume it may not
+  // be necessary as a single memory access
+  // would be good enough.
+  volatile boolean locked = false;
 }
